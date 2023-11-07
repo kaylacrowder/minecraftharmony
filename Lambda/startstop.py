0dcd052a6a947924fd30abb26c64e6c9d315e3fe
Kayla Harmon <kaylaharmon@Kaylas-MacBook-Pro.local>
2023-11-07 13:31:55 -0600

    python now has print statements

--- Lambda/startstop.py
+++ Lambda/startstop.py
@@ -6,9 +6,13 @@ def lambda_handler(event, context):
     instance_id = 'i-092264cf2d6b32574'  # Replace INSTANCE_ID with your instance ID
     instance = ec2.Instance(instance_id)
     instance_state = instance.state['Name']
+    print ({instance_state})
+    print ({instance})
 
     # Check if it's between 11pm and 8am in the current timezone
     current_time = datetime.datetime.now().time()
+    print ({current_time})
+
     #time_change = datetime.timedelta(hours=-5)
     #current_time = local_time + time_change
 
@@ -16,15 +20,15 @@ def lambda_handler(event, context):
         if instance_state == 'running':
             instance.stop()
             instance.wait_until_stopped()
-            return f'Instance stopped successfully {current_time}'
+            print (f'Instance stopped successfully {current_time}')
         #elif instance_state == 'stopped':
             #return f'Instance is already stopped {current_time}'
         else:
             if instance_state == 'stopped':
                 instance.start()
                 instance.wait_until_running()
-                return f'Instance started successfully {current_time}'
+                print (f'Instance started successfully {current_time}')
         #elif instance_state == 'running':
             #return f'Instance is already started {current_time}'
             else:
-                return f'Instance is in {instance_state} state and cannot be started or stopped {current_time}'
+                print (f'Instance is in {instance_state} state and cannot be started or stopped {current_time}')
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,49 @@
 provider "aws" {
   region = "us-west-2"
-  profile = "personal"
+  profile = "default"
 }
+
+terraform {
+  backend "s3" {
+    bucket         	   = "minecraftharmonybackup"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-west-2"
+    encrypt        	   = true
+}
+}
+/*
+#import blocks nov 2023
+import {
+  to = aws_lambda_function.instance_onoff_function
+  id = "instance_onoff_function"
+}
+import {
+  to = aws_iam_role_policy_attachment.lambda_policy_attachment_2
+
+  id = "lambda-role/arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+import {
+  to = aws_iam_role_policy_attachment.lambda_policy_attachment
+
+  id = "lambda-role/arn:aws:iam::792362287537:policy/ec2-policy"
+}
+import {
+  to = aws_iam_role.lambda_role
+
+  id = "lambda-role"
+}
+import {
+  to = aws_iam_policy.ec2_policy
+
+  id = "arn:aws:iam::792362287537:policy/ec2-policy"
+}
+import {
+  to = aws_cloudwatch_event_rule.instance_onoff_rule
+  id = "instance-onoff-rule"
+}
+import {
+  to = aws_cloudwatch_event_target.instance_start_target
+
+  id = "instance_onoff_rule/instance_onoff_lambda"
+}*/
+
